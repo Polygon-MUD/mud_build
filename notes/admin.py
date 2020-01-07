71767308f4c99f8c7f5cdd0e43378d45efabb197
Scott Renz <scottrenz@scottrenz.com>
2020-01-07 12:47:59 -0500

    completed day 2 stuff

--- notes/admin.py
+++ notes/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
+from django.db import models
 from .models import Note
-# Register your models here.
-admin.site.register(Note)
+from django.contrib.auth.models import User
+class NoteAdmin(admin.ModelAdmin):
+	readonly_fields=('created_at', 'last_modified')
 
-admin.site.register(Note)
+class PersonalNote(Note):   # Inherits from Note!
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+# Register your models here.
+admin.site.register(Note, NoteAdmin)
+# Register your models here.
 admin.site.register(PersonalNote)   # etc.
 
