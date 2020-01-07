324f236c83112b4acb2af18cc0997b7bc3f22aee
Adetunji <Ashennaike@gmail.com>
2020-01-07 15:36:03 -0500

    rooms

---
+++ rooms/tunji_rooms.py
@@ -0,0 +1,39 @@
+from django.contrib.auth.models import User
+from adventure.models import Player, Room
+
+
+r_floating_pyramids = Room(title="Floating Pyramids", description="Pyramids sitting high above the desert in Shurima")
+
+r_targon = Room(title="Mt. Targon", description="The desolate mountain with the highest peak, Targon")
+
+r_moutain_pass = Room(title="Mountain Pass", description="Mountain pass used to cross the boarder, blocked with a key locked gate")
+
+r_nefaras_necrocalis = Room(title="Nefara's Necrocalis", description="Hut laid out near the cemetary, used for Necromancy training")
+
+r_fyron_flat = Room(title="Fyron's Salt Flat", description="House belonging to Fyron the Mage, imbued with magic and made of salt")
+
+r_shurima_lake = Room(title="Lake Shurima", description="Lake surround most of Shurima, the waters shift from shallow to deep very quickly")
+
+r_velocatronic = Room(title="Velocatronic Rail Station", description="Run down station that used to house the greatest train known to Shurima")
+
+r_voodou = Room(title="Voodou Lands", description="Land owned by the Farmers of Shurima, plenty of desert fruit around to eat")
+
+r_zayite_complex = Room(title="Zayite Complex", description="School specializing in life studies, and basic magics")
+
+r_shurima = Room(title="Shurima", description="The Heavenly Kingdom of Shurima")
+
+r_azirs_palace = Room(title="Azir's Palace", description="Grand Palace belonging to a descendant of Shurima")
+
+r_royal_palace = Room(title="Royal Palace", description="Palace used by the King of Shurima")
+
+r_water_gate = Room(title="Water Gate", description="Gate nested near the edge of the lake, to prevent flooding and the releasing of monstrous sea creatures")
+
+r_tempest_forest = Room(title="Tempest Forest", description="Forest of cacti and high winds")
+
+r_centralis = Room(title="Centralis", description="Shurima's neighboring Kingdom, home to the mighty ")
+
+r_holy_temple = Room(title="Holy Temple", description="A place to worship Shurima")
+
+r_pykes_grave = Room(title="Pyke's Grave", description="The burial spot of the Pirate drowned in sand")
+
+r_nasus_statue = Room(title="Nasus' Statue", description="Statue of the first descendant of Nasus")
