b15fd9a72429eec10890464ff1977f4eba0d804c
Adetunji <Ashennaike@gmail.com>
2020-01-08 18:07:42 -0500

    api call for getting a list of all rooms and getting a single room done

--- adventure/views.py
+++ adventure/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
 from .models import *
 from .serializers import *
 
@@ -10,33 +11,23 @@ def allRooms(request):
     """
     List all rooms.
     """
-    rooms = Room.objects.all()
-    serializer = RoomSerializer(rooms, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    rooms = [{
+      "title": x.title,
+      "description": x.title,
+      "players": x.playerNames(0)} for x in Room.objects.all()]
+    return JsonResponse(rooms, safe=False)
 
 @csrf_exempt
-def singleRoom(request):
+def singleRoom(request, pk):
     """
-    Retrieve, update or delete a code snippet.
+    Retrieve 1 room.
     """
-    pass
-    # try:
-    #     snippet = Snippet.objects.get(pk=pk)
-    # except Snippet.DoesNotExist:
-    #     return HttpResponse(status=404)
-
-    # if request.method == 'GET':
-    #     serializer = SnippetSerializer(snippet)
-    #     return JsonResponse(serializer.data)
-
-    # elif request.method == 'PUT':
-    #     data = JSONParser().parse(request)
-    #     serializer = SnippetSerializer(snippet, data=data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return JsonResponse(serializer.data)
-    #     return JsonResponse(serializer.errors, status=400)
-
-    # elif request.method == 'DELETE':
-    #     snippet.delete()
-    #     return HttpResponse(status=204)
+    try:
+        item = Room.objects.get(id=pk)
+        room = {
+          "title": item.title,
+          "description": item.title,
+          "players": item.playerNames(0)}
+    except Room.DoesNotExist:
+        return HttpResponse(status=404)
+    return JsonResponse(room, safe=False)
